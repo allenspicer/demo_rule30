fcac8b8b3d633b57dbd85badd1c0ff67b644eaed
Allen Spicer - uBack <allenspicer@gmail.com>
2018-05-03 10:32:41 -0400

    Empty Xcode Project

---
+++ Demo_Rule30/Demo_Rule30/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo_Rule30
+//
+//  Created by uBack on 5/3/18.
+//  Copyright © 2018 rule30Demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
