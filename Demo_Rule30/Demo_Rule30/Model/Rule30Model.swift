b0a0ba41cbdd472d24dfe5a3e0c7ca76ce5240cc
Allen Spicer - uBack <allenspicer@gmail.com>
2018-05-03 17:48:08 -0400

    Rule 30 Demonstration Completed. Two known issues: 1. scrolling creates row duplication. 2. edge cases defaulting to false encroaches on results

--- Demo_Rule30/Demo_Rule30/Model/Rule30Model.swift
+++ Demo_Rule30/Demo_Rule30/Model/Rule30Model.swift
@@ -14,17 +14,45 @@ class Rule30Model : Rule30ModelProtocol {
     var data = [Int:[Bool]]()
     
     func generateRowsAndReturnCount () -> Int{
-        
-        data = [ 0 : [Bool](repeating: false, count: 10) + [true] + [Bool](repeating: false, count: 10)]
-        
+        data = generateRows(numberOfRowsToGenerate: 49, numberOfColumnsToGenerate: 21)
         return data.count
     }
 
     func getRow(_ rowIndex :Int ) -> [Bool]?{
         
-        return data[rowIndex]
-        
         //check that the requested index is contained in the dataset
+        if rowIndex < data.count - 1{
+            if let row = data[rowIndex]{
+                return row
+            }
+        }
+        return nil
+    }
+    
+    func generateRows( numberOfRowsToGenerate: Int, numberOfColumnsToGenerate: Int) -> [Int:[Bool]]{
+        
+        // if data has not already been generated
+        if (data.count == 0){
+        
+            //split total to create a first row with just one true value in the middle
+            //and all other values set to false
+            let oneSideOfColumns = (numberOfColumnsToGenerate - 1) / 2
+            let firstRow = [ 0 : [Bool](repeating: false, count: oneSideOfColumns) + [true] + [Bool](repeating: false, count: oneSideOfColumns)]
+            data = firstRow
+            
+            //generate rows after this inital row
+            for x in 1...numberOfRowsToGenerate{
+                var rowArray = [Bool]()
+                
+                for y in 0...numberOfColumnsToGenerate{
+                    guard let previousRow = data[x-1] else {return data}
+                    guard let cellBoolValue = isCellTrue(previousRow, indexForCell: y) else {return data}
+                    rowArray.append(cellBoolValue)
+                }
+                data[x] = rowArray
+            }
+        }
+        return data
     }
     
     func isCellTrue(_ previousRowArrayOfBools : [Bool], indexForCell : Int ) -> Bool?{
@@ -45,6 +73,7 @@ class Rule30Model : Rule30ModelProtocol {
         if (a && b && c){ return false}
         
         //all other scenarios
+        if (a && b && !c){ return false}
         if (a && !b && c){ return false}
         if (a && !b && !c){ return true}
         if (!a && b && c){ return true}
