58d7a9e7ddf3a2ea75cc077ab1aaab7573e71f44
Allen Spicer - uBack <allenspicer@gmail.com>
2019-05-14 20:23:25 -0400

    Expanded setup of tests. Cleaned unused tests. Improved test logic.

--- Demo_Rule30/Tests/Demo_Rule30Tests.swift
+++ Demo_Rule30/Tests/Demo_Rule30Tests.swift
@@ -2,7 +2,7 @@
 //  Demo_Rule30Tests.swift
 //  Demo_Rule30Tests
 //
-//  Created by uBack on 5/3/18.
+//  Created by Allen Spicer on 5/3/18.
 //  Copyright © 2018 rule30Demo. All rights reserved.
 //
 
@@ -11,9 +11,17 @@ import XCTest
 
 class Demo_Rule30Tests: XCTestCase {
     
+    var model : Rule30Model!
+    var vc : ViewController!
+
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        model = Rule30Model()
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        vc = storyboard.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        
     }
     
     override func tearDown() {
@@ -21,21 +29,7 @@ class Demo_Rule30Tests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
-    
     func test_model_returns_false_scenario_one(){
-        let model = Rule30Model()
         let bool = model.isCellTrue([true,true,true], indexForCell: 1)
         XCTAssert(bool == false)
     }
