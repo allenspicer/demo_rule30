213b5a9fddff6bdd96eb491fcbd72421aa8e9d44
Allen Spicer - uBack <allenspicer@gmail.com>
2019-05-16 22:01:03 -0400

    Color Picker View Updates and Tests

---
+++ Demo_Rule30/Tests/Demo_Rule30_CPV_Tests.swift
@@ -0,0 +1,42 @@
+//
+//  Demo_Rule30_VC_Tests.swift
+//  Demo_Rule30_VC_Tests
+//
+//  Created by Allen Spicer on 5/3/18.
+//  Copyright © 2018 rule30Demo. All rights reserved.
+//
+
+import XCTest
+@testable import Demo_Rule30
+
+class Demo_Rule30_CPV_Tests: XCTestCase {
+    
+    var colorPickerView : ColorPickerView!
+    
+    override func setUp() {
+        super.setUp()
+        colorPickerView = ColorPickerView()
+    }
+    
+    override func tearDown() {
+        colorPickerView = nil
+        super.tearDown()
+    }
+
+    func test_color_update(){
+        //GIVEN
+        let touch = UITouch()
+        
+        //WHEN
+        colorPickerView.updateColor(touch: touch)
+        
+        
+        //THEN
+        
+        
+    }
+    
+    
+    
+    
+}
